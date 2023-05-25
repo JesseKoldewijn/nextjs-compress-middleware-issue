c8fbca87bc62004e73f8d9b5f8094045171d2d2d
Jesse Koldewijn <jesse@iungo.nl>
2023-05-25 11:04:32 +0200

    init: creation of repo with reproduction

---
+++ README.md
@@ -0,0 +1,1 @@
+# Reproduction of issue #50320
---
+++ src/middleware.ts
@@ -0,0 +1,10 @@
+import { type NextRequest, NextResponse } from 'next/server';
+ 
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+    console.log(request);
+    
+    const response = NextResponse.next();
+   
+    return response;
+  }
